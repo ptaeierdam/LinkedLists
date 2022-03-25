e47811c7c34e7b383c54f895336251b9861ebf34
ptaeierdam <ptaeierdam@gmail.com>
2022-03-24 23:05:06 -0700

    Initial commit

---
+++ src/main/java/SingleDriver.java
@@ -0,0 +1,45 @@
+public class SingleDriver {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> linkedList = new SinglyLinkedList<>();
+
+        System.out.println(linkedList.contains(null));
+
+        linkedList.addFirst("First");
+        linkedList.addFirst("Second");
+        linkedList.addFirst("Third");
+        linkedList.addFirst("Fourth");
+        linkedList.addFirst("Fifth");
+
+        linkedList.addLast("Sixth");
+        linkedList.addLast("Seventh");
+        linkedList.addLast("Eighth");
+
+        System.out.println(linkedList.contains("First"));
+        System.out.println(linkedList.contains("Eighth"));
+        System.out.println(linkedList.contains("Tenth"));
+        System.out.println(linkedList.contains(null));
+
+        System.out.println(linkedList);
+
+        System.out.println("Head: " + linkedList.peekFirst());
+        System.out.println("Tail: " + linkedList.peekLast());
+
+        System.out.println("Poll First: " + linkedList.pollFirst());
+        System.out.println("Poll Last: " + linkedList.pollLast());
+
+        System.out.println("Head: " + linkedList.peekFirst());
+        System.out.println("Tail: " + linkedList.peekLast());
+
+        System.out.println(linkedList);
+
+        System.out.println(linkedList.size());
+
+        linkedList.clear();
+
+        System.out.println(linkedList.size());
+
+    }
+
+}
